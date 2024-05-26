3e4c6e8bf6206759d8b449b02de94631f27a6a3a
edlaaaaa <anamshkhan5@gmail.com>
2024-05-26 07:54:03 +0000

    solution

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import AppShowcase from './components/AppShowcase'
+
+import './App.css'
+
+const App = () => <AppShowcase />
+
+export default App
---
+++ src/components/AppShowcase/index.js
@@ -0,0 +1,18 @@
+import {Component} from 'react'
+import './index.css'
+
+class AppShowcase extends Component {
+  render() {
+    return (
+      <div className="mobile-app-container">
+        <h1 className="heading">
+          Best Mobile <br /> App Showcase
+        </h1>
+        <p className="detail">vjhbj khbijhi kljmk</p>
+        <button className="read">Read More</button>
+      </div>
+    )
+  }
+}
+
+export default AppShowcase
